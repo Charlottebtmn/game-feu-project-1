e624ec353fc3af5c2c8cfcd265ca9ac324d93e25
Charlottebtmn <c.bottemanne@gmail.com>
2017-12-12 16:03:21 +0100

    review 6

--- feu.js
+++ feu.js
@@ -46,7 +46,7 @@ function Game () {
     that.changeScreen();
     setTimeout(function(){
          s.play();
-       }, 3000);
+       }, 5000);
     that.countDown(5, function() {
       that.status ="waitingResult";
     });
@@ -64,7 +64,7 @@ function Game () {
     that.status = "inCountDown5";
     setTimeout(function(){
          s.play();
-       }, 3000);
+       }, 5000);
     that.updateTeamNames();
     that.finished();
     that.changeScreen();
